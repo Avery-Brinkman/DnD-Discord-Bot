9b4321e5e2c695136cb078b56d2ee236d93b4382
Avery Brinkman <brinkmae@mail.uc.edu>
2021-04-20 14:26:51 -0400

    Added advantage and disadvantage

--- Bot_Code.py
+++ Bot_Code.py
@@ -14,8 +14,19 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('$roll '):
+    if message.content.lower().startswith('$roll a '):
+        msgContent = message.content[8:]
+        await message.channel.send(max(roll(msgContent, True), roll(msgContent, True)))
+        return
+    
+    if message.content.lower().startswith('$roll d '):
+        msgContent = message.content[8:]
+        await message.channel.send(min(roll(msgContent, True), roll(msgContent, True)))
+        return
+
+    if message.content.lower().startswith('$roll '):
         msgContent = message.content[6:]
         await message.channel.send(roll(msgContent, True))
+        return
 
 client.run('ODMzMDU0NzUyODQ5NjU3ODc2.YHswsg.FE2KVBTo9hJX6k3EFKMJoJJdeP4')
